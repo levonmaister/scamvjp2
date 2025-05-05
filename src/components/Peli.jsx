ebfc9809a1dab1520e2c2f6cd001ccdf8b42dcab
gabrane1 <emelie.gabran@aalto.fi>
2025-05-05 18:32:55 +0300

    spel funkar, inte jättevackert

--- src/components/Peli.jsx
+++ src/components/Peli.jsx
@@ -12,6 +12,9 @@ export default function ViestiPeli() {
   const [step, setStep] = useState(0);
   const [feedback, setFeedback] = useState(null); 
   const [locked, setLocked] = useState(false);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+
   
   const current = script[step];
   
@@ -19,31 +22,61 @@ export default function ViestiPeli() {
     if (locked) return;
     setLocked(true);
     setFeedback(option.correct ? "Correct!" : "Wrong!");
+
+    if (option.correct) {
+      setScore((prevScore) => prevScore + 1)
+    }
+
     setTimeout(() => {
-      setStep((prev) => prev + 1);
+      if (step + 1 < script.length) {
+        setStep((prev) => prev + 1);
+      } else {
+        setGameOver(true)
+      }      
       setFeedback(null);
       setLocked(false);
     }, 1200);
   };
+
+  const playAgain = () => {
+    setStep(0);
+    setScore(0);
+    setGameOver(false);
+  };
   
+  if (gameOver) {
+    return (
+      <div className="result">
+        <h4>Peli loppu!</h4>
+        <p>Tunnistit: {score}/{script.length} hujausta</p>
+        <button onClick={playAgain}>Pelaa uudestaan</button>
+
+      </div>
+    )
+  };
     
   return (
     <div className="chat-container">
-      <div className="sender">{current.sender}</div>
-      <div className="bubble left">{current.message}</div>
-
-      {current.options.length > 0 && (
+      <h1>Tunnista huijausviestit!</h1>
+      <h3>Näin pelaat: lue viesti, jos epäilet viestin olevan huijaus, paina "Huijaus"-nappia. Paina "Aito"-nappia, jos viesti on mielestäsi luotettava.</h3>
+      
+      <div className="chat">
+        <div className="sender">{current.sender}</div>
+        <div className="bubble left">{current.message}</div>
+        <div className="fBack">
+        {feedback && <div className={`feedback ${feedback === "Correct!" ? "correct" : "wrong"}`}>{feedback}</div>}
+        </div>
+        <div className="keyboard"></div>
         <div className="options">
           {current.options.map((opt, i) => (
             <button key={i} onClick={() => handleChoice(opt)} disabled={locked}>
-              {opt.text}
+              {opt.answer}
             </button>
           ))}
         </div>
-      )}
-
-      {feedback && <div className={`feedback ${feedback === "Correct!" ? "correct" : "wrong"}`}>{feedback}</div>}
+      </div>
     </div>
+    
   );
   
-}
+};
