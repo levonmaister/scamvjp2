3b5ae2661c4b6268e7e6cee3f302379dd634cd40
kattilr1 <robin.kattilakoski@aalto.fi>
2025-05-12 21:16:40 +0300

    fixat på footern och tillagt vissa logon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import { Link } from "react-router-dom"; 
-import '../styles/Footer.css'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import mailIcon from "../pictures/mail-logo.png";
+import igIcon from "../pictures/ig-logo.png";
+import fbIcon from "../pictures/facebook.png";
+import "../styles/Footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer-content">
-                <div className="footer-links">
-                    <a href="mailto:tulitkohuijatuksi@gmail.com">Ota yhteyttä</a>
-                    <Link to="/ilmoitus">Ilmoita huijaus</Link>
-                    <a href="https://www.instagram.com/tulitkohuijatuksi/"target="_blank">Instagram</a>
-                    <a href="https://m.facebook.com/profile.php?id=61576197118855&name=xhp_nt__fb__action__open_user"target="_blank">Facebook</a>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bottom-fill" id="contact">
+      <div className="socials-logo-text">
+        <Link to="/ilmoitus" className="ilmoitus-link">
+          Ilmoita huijaus
+        </Link>
+
+        <img src={mailIcon} alt="Sähköposti" />
+        <a href="mailto:tulitkohuijatuksi@gmail.com">Ota yhteyttä</a>
+
+        <img src={igIcon} alt="Instagram" />
+        <a
+          href="https://www.instagram.com/tulitkohuijatuksi/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @tulitkohuijatuksi
+        </a>
+
+        <img src={fbIcon} alt="Facebook" />
+        <a
+          href="https://m.facebook.com/profile.php?id=61576197118855&name=xhp_nt__fb__action__open_user"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Facebook
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
