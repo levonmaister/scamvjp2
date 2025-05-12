642069786b4d1f1cb95a1c2c7c5aa7e06dd89655
kattilr1 <robin.kattilakoski@aalto.fi>
2025-05-12 21:34:45 +0300

    fixa på footern vidare o tillsatt logon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,23 @@ import { Link } from "react-router-dom";
 import mailIcon from "../pictures/mail-logo.png";
 import igIcon from "../pictures/ig-logo.png";
 import fbIcon from "../pictures/facebook.png";
+import logo from "../pictures/LOGO.svg";
+
 import "../styles/Footer.css";
 
-const Footer = () => {
-  return (
-    <footer className="bottom-fill" id="contact">
-      <div className="socials-logo-text">
-        <Link to="/ilmoitus" className="ilmoitus-link">
-          Ilmoita huijaus
-        </Link>
+const Footer = () => (
+  <footer className="footer" id="contact">
+    <div className="footer-links">
+      <Link to="/ilmoitus" className="ilmoitus-link">
+        Ilmoita huijaus
+      </Link>
 
+      <div className="social-item">
         <img src={mailIcon} alt="Sähköposti" />
         <a href="mailto:tulitkohuijatuksi@gmail.com">Ota yhteyttä</a>
+      </div>
 
+      <div className="social-item">
         <img src={igIcon} alt="Instagram" />
         <a
           href="https://www.instagram.com/tulitkohuijatuksi/"
@@ -24,7 +28,9 @@ const Footer = () => {
         >
           @tulitkohuijatuksi
         </a>
+      </div>
 
+      <div className="social-item">
         <img src={fbIcon} alt="Facebook" />
         <a
           href="https://m.facebook.com/profile.php?id=61576197118855&name=xhp_nt__fb__action__open_user"
@@ -34,8 +40,15 @@ const Footer = () => {
           Facebook
         </a>
       </div>
-    </footer>
-  );
-};
+    </div>
+
+    <div className="address-container">
+      <p className="address">Otakaari 1</p>
+      <p className="address">02150 Espoo</p>
+    </div>
+
+    <img src={logo} alt="Tulitko Huijatuksi -logo" className="bottom-logo" />
+  </footer>
+);
 
 export default Footer;
